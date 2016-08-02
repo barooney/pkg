bedbe062a8d315872c3835f0bbcc5f1cdf653526
Daniel Baron <daniel@barooney.com>
2016-08-03 01:36:00 +0200

    fixed the screenshot and version parsing.

--- src/main.c
+++ src/main.c
@@ -7,110 +7,132 @@
 #include <jansson.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <malloc.h>
 #include <string.h>
 
 #include "graphics.h"
 
 struct PKGVersion {
-	char* version;
-	char* url;
-	PKGVersion* next;
+    char* version;
+    char* url;
 };
 
 struct PKGHomebrew {
-	char* name;
-	char* title_id;
-	char* description;
-	char* icon;
-	char** screenshots;
-	PKGVersion** versions;
+    char*        name;
+    char*        title_id;
+    char*        description;
+    char*        icon;
+    char*        latest;
+    int          numScreenshots;
+    char**       screenshots;
+    int          numVersions;
+    PKGVersion** versions;
 };
 
-void read_homebrews( json_t *json ) {
-	int size = json_array_size( json );
-	psvDebugScreenPrintf("JSON array contains %d homebrews.\n\n", size );
-	struct PKGHomebrew homebrews[size];
-
-	size_t i;
-	json_t *value;
-	json_t *jsonHomebrew;
-	json_array_foreach( json, i, jsonHomebrew ) {
-
-		psvDebugScreenPrintf( "addr: 0x%08X with index %d\n", jsonHomebrew, i );
-		
-		// general info
-		homebrews[i].name        = ( char* ) json_string_value( json_object_get( jsonHomebrew, "name" ) );
-		homebrews[i].title_id    = ( char* ) json_string_value( json_object_get( jsonHomebrew, "TITLEID" ) );
-		psvDebugScreenPrintf( "Found homebrew %s (%s)\n", homebrews[i].name, homebrews[i].title_id );
-
-		homebrews[i].description = ( char* ) json_string_value( json_object_get( jsonHomebrew, "description" ) );
-		psvDebugScreenPrintf( "Description: %s\n", homebrews[i].description );
-
-		homebrews[i].icon        = ( char* ) json_string_value( json_object_get( jsonHomebrew, "icon" ) );
-		psvDebugScreenPrintf( "Icon:        %s\n", homebrews[i].icon );
-
-		// screenshots
-		size_t screenshotCounter = 0;
-		json_t* jsonScreenshots = json_object_get( jsonHomebrew, "screenshots" );
-		json_t* jsonScreenshot;
-		int screenShotLength = json_array_size( jsonScreenshots );
-		psvDebugScreenPrintf( "number of screenshots: %d\n", screenShotLength );
-
-		psvDebugScreenPrintf( "homebrews[%d].screenshots, length = %d\n", i, screenShotLength );
-		homebrews[i].screenshots = new char *[screenShotLength];
-		json_array_foreach( jsonScreenshots, screenshotCounter, jsonScreenshot ) {
-			homebrews[i].screenshots[screenshotCounter] = ( char* ) json_string_value( jsonScreenshot );
-			psvDebugScreenPrintf( "%s\n", homebrews[i].screenshots[screenshotCounter] );
-		}
-
-		// versions
-		size_t versionCounter = 0;
-		json_t* jsonVersions = json_object_get( jsonHomebrew, "versions" );
-		int versionLength = json_object_size( jsonVersions );
-		psvDebugScreenPrintf( "number of versions: %d\n", versionLength );
-
-		homebrews[i].versions = new PKGVersion *[versionLength];
-		
-		const char *versionNumber;
-		json_t *versionUrl;
-		json_object_foreach( jsonVersions, versionNumber, versionUrl ) {
-			homebrews[i].versions[versionCounter]->version = ( char* ) versionNumber;
-			homebrews[i].versions[versionCounter]->url     = ( char* ) json_string_value( versionUrl );
-			if ( versionCounter > 0 ) {
-				homebrews[i].versions[versionCounter - 1]->next = homebrews[i].versions[versionCounter];
-			}
-			psvDebugScreenPrintf( "%s %s\n", homebrews[i].versions[versionCounter]->version, homebrews[i].versions[versionCounter]->url );
-			versionCounter++;
-		}
-		psvDebugScreenPrintf( "\n");
-	}
+int get_number_of_homebrews( json_t *json ) {
+    int size = json_array_size( json );
+    return size;
 }
 
-int main(int argc, char *argv[]) {
-	psvDebugScreenInit();
-	psvDebugScreenPrintf("JSON Sample v.1.0 by barooney\n\n");
+char *get_latest_url( PKGHomebrew &hb ) {
+    int versionCount = hb.numVersions;
+    for ( int y = 0; y < versionCount; y++ ) {
+        if ( 0 == strcmp( hb.versions[ y ]->version, hb.latest ) ) {
+            return hb.versions[ y ]->url;
+        }
+    }
+    return NULL;
+}
 
-	json_t *json;
+void get_homebrews( json_t *json, PKGHomebrew *homebrews ) {
+    size_t i;
+    json_t *value;
+    json_t *jsonHomebrew;
+    json_array_foreach( json, i, jsonHomebrew ) {
+
+        // general info
+        homebrews[i].name        = ( char* ) json_string_value( json_object_get( jsonHomebrew, "name" ) );
+        homebrews[i].title_id    = ( char* ) json_string_value( json_object_get( jsonHomebrew, "TITLEID" ) );
+        homebrews[i].description = ( char* ) json_string_value( json_object_get( jsonHomebrew, "description" ) );
+        homebrews[i].latest      = ( char* ) json_string_value( json_object_get( jsonHomebrew, "latest" ) );
+        homebrews[i].icon        = ( char* ) json_string_value( json_object_get( jsonHomebrew, "icon" ) );
+
+        // screenshots
+        size_t screenshotCounter = 0;
+        json_t* jsonScreenshots = json_object_get( jsonHomebrew, "screenshots" );
+        json_t* jsonScreenshot;
+        homebrews[ i ].numScreenshots = json_array_size( jsonScreenshots );
+        homebrews[ i ].screenshots = new char *[ homebrews[ i ].numScreenshots ];
+        json_array_foreach( jsonScreenshots, screenshotCounter, jsonScreenshot ) {
+            homebrews[ i ].screenshots[ screenshotCounter ] = ( char* ) json_string_value( jsonScreenshot );
+        }
+
+        // versions
+        size_t versionCounter = 0;
+        json_t* jsonVersions = json_object_get( jsonHomebrew, "versions" );
+        const char *versionNumber;
+        json_t *versionUrl;
+        homebrews[ i ].numVersions = json_object_size( jsonVersions );
+        homebrews[ i ].versions = new PKGVersion *[ homebrews[ i ].numVersions ];
+        json_object_foreach( jsonVersions, versionNumber, versionUrl ) {
+            PKGVersion *v = new PKGVersion;
+            v->version = ( char* ) versionNumber;
+            v->url = ( char* ) json_string_value( versionUrl );
+            homebrews[ i ].versions[ versionCounter ] = v;
+            versionCounter++;
+        }
+    }
+}
+
+int main( int argc, char *argv[] ) {
+    psvDebugScreenInit();
+    psvDebugScreenPrintf( "JSON Sample v.1.0 by barooney\n\n" );
+
+    json_t *json;
     json_error_t error;
 
     size_t count;
 
-    json = json_load_file("ux0:temp/sample.json", 0, &error);
+    json = json_load_file( "ux0:temp/sample.json", 0, &error );
     if ( ! json ) {
-        psvDebugScreenPrintf("Error while loading %s:\n");
-        psvDebugScreenPrintf("%s:\n", error.text);
+        psvDebugScreenPrintf( "Error while loading:\n" );
+        psvDebugScreenPrintf( "%s:\n", error.text );
     }
 
     if ( JSON_ARRAY != json_typeof( json ) ) {
-		psvDebugScreenPrintf("Error: JSON is not an Array.\n");
+        psvDebugScreenPrintf( "Error: JSON is not an Array.\n" );
     } else {
-    	read_homebrews( json );
+        int size = get_number_of_homebrews( json );
+        struct PKGHomebrew homebrews[ size ];
+        get_homebrews( json, homebrews );
+
+        psvDebugScreenPrintf( "-----------------------------------------\n" );
+        for ( int x = 0; x < size; x++ ) {
+            psvDebugScreenPrintf( "%s\n",                 homebrews[ x ].name );
+            psvDebugScreenPrintf( "TITLE_ID:       %s\n", homebrews[ x ].title_id );
+            psvDebugScreenPrintf( "Description:    %s\n", homebrews[ x ].description );
+            psvDebugScreenPrintf( "Icon:           %s\n", homebrews[ x ].icon );
+            psvDebugScreenPrintf( "Latest version: %s\n", homebrews[ x ].latest );
+            psvDebugScreenPrintf( "Latest URL:     %s\n", get_latest_url( homebrews[ x ] ) );
+            psvDebugScreenPrintf( "\n" );
+
+            for ( int y = 0; y < homebrews[ x ].numScreenshots; y++ ) {
+            psvDebugScreenPrintf( "Screenshot:     %s\n", homebrews[ x ].screenshots[ y ] );
+            }
+
+            psvDebugScreenPrintf( "\n" );
+
+            for ( int y = 0; y < homebrews[ x ].numVersions; y++ ) {
+            psvDebugScreenPrintf( "Version:        %s\n", homebrews[ x ].versions[ y ]->version );
+            psvDebugScreenPrintf( "URL:            %s\n", homebrews[ x ].versions[ y ]->url );
+            }
+            psvDebugScreenPrintf("-----------------------------------------\n");
+        }
     }
+    sceKernelDelayThread(10*1000*1000);
 
-	psvDebugScreenPrintf("This app will close in 10 seconds!\n");
-	sceKernelDelayThread(10*1000*1000);
-
-	sceKernelExitProcess(0);
-	return 0;
+    sceKernelExitProcess(0);
+    return 0;
 }
